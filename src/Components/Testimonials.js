718081af21719bbd1a0399322176c3bffc429470
Saadat Taaseen <taaseen.homework@gmail.com>
2020-09-12 03:08:36 -0400

    commited from a parent directory

--- src/Components/Testimonials.js
+++ src/Components/Testimonials.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
 
 class Testimonials extends Component {
-  render() {
+    render() {
 
-    if(this.props.data){
-      var testimonials = this.props.data.testimonials.map(function(testimonials){
-        return  <li key={testimonials.user}>
-            <blockquote>
-               <p>{testimonials.text}</p>
-               <cite>{testimonials.user}</cite>
-            </blockquote>
-         </li>
-      })
-    }
+        if (this.props.data) {
+            var testimonials = this.props.data.testimonials.map(function (testimonials) {
+                return <li key={testimonials.user}>
+                    <blockquote>
+                        <p>{testimonials.text}</p>
+                        <cite>{testimonials.user}</cite>
+                    </blockquote>
+                </li>
+            })
+        }
 
-    return (
-      <section id="testimonials">
-      <div className="text-container">
+        return (
+            <section id="testimonials">
+                {/* //!Activate testimonial on navigation bar as well */}
+                {/* <div className="text-container">
          <div className="row">
 
             <div className="two columns header-col">
@@ -29,10 +30,10 @@ class Testimonials extends Component {
                   </ul>
                </div>
             </div>
-         </div>
-   </section>
-    );
-  }
+         </div> */}
+            </section>
+        );
+    }
 }
 
 export default Testimonials;
