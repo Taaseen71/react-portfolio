92af938bdfaea57f86c13a191ff9e7759dec19be
Saadat Taaseen <taaseen.homework@gmail.com>
2020-09-13 01:46:39 -0400

    reinstalled package-lock and node modules

---
+++ build/precache-manifest.9af0a652913bc847a6f175527f8590e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9960173abbc752dc9a115c2ae9ab995",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07d0c50ac2ae762a6dce",
+    "url": "/static/css/main.a521518f.chunk.css"
+  },
+  {
+    "revision": "599e6df1e5eab4dd5a5c",
+    "url": "/static/js/2.7a32a8b1.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.7a32a8b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07d0c50ac2ae762a6dce",
+    "url": "/static/js/main.67543763.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
